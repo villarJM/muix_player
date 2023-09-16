15c13507273b44d4c0ab39427fe8673d1ea34196
Misael Villar <villarmisael7@gmail.com>
2023-09-16 16:05:09 -0600

    implement carousel slide demo

--- lib/presentation/screen/dashboard/dashboard_player.dart
+++ lib/presentation/screen/dashboard/dashboard_player.dart
@@ -1,3 +1,4 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 
 class DashboardPlayer extends StatelessWidget {
@@ -12,14 +13,49 @@ class DashboardPlayer extends StatelessWidget {
             SizedBox(
               width: double.infinity,
               child: Padding(
-                padding: EdgeInsets.fromLTRB(10, 90, 0, 0),
-                child: Text('New Single', textAlign: TextAlign.left,)),
+                  padding: EdgeInsets.fromLTRB(10, 90, 0, 0),
+                  child: Text(
+                    'New Single',
+                    textAlign: TextAlign.left,
+                  )),
             ),
             // Card New Single Music
-            _CardNewMusic(),
+            SizedBox(height: 10,),
+            CarouselCardNewMusic()
           ],
         ),
       ),
+    );
+  }
+}
+
+class CarouselCardNewMusic extends StatelessWidget {
+  const CarouselCardNewMusic({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return CarouselSlider(
+      options: CarouselOptions(
+        height: 200.0,
+        enlargeCenterPage: true,
+        viewportFraction: 1.0
+      ),
+      items: [1, 2, 3, 4, 5].map((i) {
+        return Builder(
+          builder: (BuildContext context) {
+            return Container(
+                width: MediaQuery.of(context).size.width,
+                margin: const EdgeInsets.symmetric(horizontal: 10.0),
+                decoration: BoxDecoration(color: Colors.amber,borderRadius: BorderRadius.circular(10)),
+                child: Text(
+                  'text $i',
+                  style: const TextStyle(fontSize: 16.0),
+                ));
+          },
+        );
+      }).toList(),
     );
   }
 }
@@ -34,16 +70,16 @@ class _CardNewMusic extends StatelessWidget {
       child: SizedBox(
         height: 200,
         child: Card(
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(10)),
+            shape:
+                RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
             margin: const EdgeInsets.all(10),
             elevation: 10,
-          
+
             // Dentro de esta propiedad usamos ClipRRect
             child: ClipRRect(
               // Los bordes del contenido del card se cortan usando BorderRadius
               borderRadius: BorderRadius.circular(10),
-          
+
               // EL widget hijo que será recortado segun la propiedad anterior
               child: const Column(
                 children: <Widget>[
@@ -54,7 +90,7 @@ class _CardNewMusic extends StatelessWidget {
                     // Como queremos traer una imagen desde un url usamos NetworkImage
                     image: NetworkImage(
                         'https://www.yourtrainingedge.com/wp-content/uploads/2019/05/background-calm-clouds-747964.jpg'),
-                        fit: BoxFit.cover,
+                    fit: BoxFit.cover,
                   ),
                 ],
               ),
