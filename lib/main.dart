0bd7a816317602af1aa610241004bbc1d60dd0a2
Misael Villar <villarmisael7@gmail.com>
2023-09-16 15:25:48 -0600

    implementation dashboard player demo

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:muix_player/presentation/screen/dashboard/dashboard_player.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,11 +11,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: Scaffold(appBar: AppBar(title: const Text('Muix Player'),)),
+      home: const DashboardPlayer(),
     );
   }
 }
