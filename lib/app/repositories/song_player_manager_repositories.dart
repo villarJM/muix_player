94cd25d30519a71d8c4819d4ae7db3ffcbf9388f
Misael Villar <villarmisael7@gmail.com>
2023-10-02 20:10:11 -0600

    implement try catch in player_manager

--- lib/app/repositories/song_player_manager_repositories.dart
+++ lib/app/repositories/song_player_manager_repositories.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:audioplayers/audioplayers.dart';
 
 class SongPlayerManager {
@@ -6,7 +8,13 @@ class SongPlayerManager {
       audioPlayer.onPositionChanged;
 
   Future<void> playLocalAudio(String path) async {
-    await audioPlayer.play(DeviceFileSource(path));
+    try {
+      await audioPlayer.play(DeviceFileSource(path));
+    } on FileSystemException catch (e) {
+      throw FileSystemException('there was an error trying to read the file: $e');
+    } on IOException catch (e) {
+      e.toString();
+    }
   }
 
   Future<void> pauseLocalAudio() async {
