a1ebd82f2df996a2f5d7d3bc9e5e6c8aadb591d2
Misael Villar <villarmisael7@gmail.com>
2024-04-29 13:13:26 -0600

    reajust controller

--- lib/presentation/screen/all_songs/songs.dart
+++ lib/presentation/screen/all_songs/songs.dart
@@ -26,7 +26,6 @@ const Songs({ Key? key }) : super(key: key);
 class _SongsState extends ConsumerState<Songs> with AutomaticKeepAliveClientMixin {
 
   SampleItem? selectedItem;
-  ScrollController scrollController = ScrollController();
   final audioManager = getIt<AudioManager>();
   final offlineSongLocal = getIt<OfflineSongLocal>();
   final dominateColor = DominateColor();
@@ -44,66 +43,62 @@ class _SongsState extends ConsumerState<Songs> with AutomaticKeepAliveClientMixi
       child: ValueListenableBuilder<List<MediaItem>>(
         valueListenable: audioManager.playlistNotifier,
         builder: (context, playlistSongs,_) {
-          return OrientationBuilder(
-            builder: (context, orientation) {
-              return ListView.builder(
-                primary: true,
-                scrollDirection: Axis.vertical,
-                itemCount: playlistSongs.length,
-                addAutomaticKeepAlives: true,
-                itemBuilder: (context, index) {
-                  final songs = playlistSongs[index];
-                  return ListItem(
-                    height: orientation == Orientation.portrait ? 45.h : 50.h,
-                    title: Text(songs.title, maxLines: 1, style: AppMuixTheme.textTitleUrbanistRegular16,),
-                    subtitle: Text(songs.artist ?? "", maxLines: 1, style: AppMuixTheme.textUrbanistBold16,),
-                    artwork: LoadArtwork(
-                      id: int.parse(songs.id), 
-                      artworkType: ArtworkType.AUDIO,
-                      height: 100.h,
-                      frameBuilder: (context, child, frame, wasSynchronouslyLoaded) {
-                        if (wasSynchronouslyLoaded) return child;
-                        return AnimatedOpacity(
-                          opacity: frame == null ? 0 : 1,
-                          duration: const Duration(milliseconds: 500),
-                          curve: Curves.easeOut,
-                          child: child,
-                        );
-                      },
-                    ),
-                    onTap: () {
-                      ref.watch(colorStateProvider.notifier).getDominantingColorImage(int.parse(songs.id), ArtworkType.AUDIO, 200);
-                      audioManager..skipToNextQueueItem(index)..play();
-                    },
-                    icon: popupMenuButton(context, int.parse(songs.id)), 
-                      
-                    boxDecoration: BoxDecoration(
-                      border: Border.all(
-                        color: Colors.white
-                      ),
-                      borderRadius: BorderRadius.only(
-                        topLeft: index % 2 == 0 ? const Radius.circular(20) : Radius.zero,
-                        topRight: index % 2 == 0 ? Radius.zero : const Radius.circular(20),
-                        bottomLeft: index % 2 == 0 ? Radius.zero : const Radius.circular(20),
-                        bottomRight: index % 2 == 0 ? const Radius.circular(20) : Radius.zero,
-                      ),
-                      // gradient: const LinearGradient(
-                      //   colors: [Color.fromARGB(255, 5, 23, 39), Color.fromARGB(200, 228, 211, 182),Color.fromARGB(255, 228, 211, 182),],
-                      //   begin: Alignment.centerLeft,
-                      //   end: Alignment.centerRight,
-                      // )
-                    ),
-                    imageBorderRadius: BorderRadius.only(
-                      topLeft: index % 2 == 0 ? const Radius.circular(19) : Radius.zero,
-                      topRight: index % 2 == 0 ? Radius.zero : const Radius.circular(19),
-                      bottomLeft: index % 2 == 0 ? Radius.zero : const Radius.circular(19),
-                      bottomRight: index % 2 == 0 ? const Radius.circular(19) : Radius.zero,
-                    ),
-                  );
-                  
+          return ListView.builder(
+            primary: true,
+            scrollDirection: Axis.vertical,
+            itemCount: playlistSongs.length,
+            addAutomaticKeepAlives: true,
+            itemBuilder: (context, index) {
+              final songs = playlistSongs[index];
+              return ListItem(
+                height: 45.h,
+                title: Text(songs.title, maxLines: 1, style: AppMuixTheme.textTitleUrbanistRegular16,),
+                subtitle: Text(songs.artist ?? "", maxLines: 1, style: AppMuixTheme.textUrbanistBold16,),
+                artwork: LoadArtwork(
+                  id: int.parse(songs.id), 
+                  artworkType: ArtworkType.AUDIO,
+                  height: 100.h,
+                  frameBuilder: (context, child, frame, wasSynchronouslyLoaded) {
+                    if (wasSynchronouslyLoaded) return child;
+                    return AnimatedOpacity(
+                      opacity: frame == null ? 0 : 1,
+                      duration: const Duration(milliseconds: 500),
+                      curve: Curves.easeOut,
+                      child: child,
+                    );
+                  },
+                ),
+                onTap: () {
+                  ref.watch(colorStateProvider.notifier).getDominantingColorImage(int.parse(songs.id), ArtworkType.AUDIO, 200);
+                  audioManager..skipToNextQueueItem(index)..play();
                 },
+                icon: popupMenuButton(context, int.parse(songs.id)), 
+                  
+                boxDecoration: BoxDecoration(
+                  border: Border.all(
+                    color: Colors.white
+                  ),
+                  borderRadius: BorderRadius.only(
+                    topLeft: index % 2 == 0 ? const Radius.circular(20) : Radius.zero,
+                    topRight: index % 2 == 0 ? Radius.zero : const Radius.circular(20),
+                    bottomLeft: index % 2 == 0 ? Radius.zero : const Radius.circular(20),
+                    bottomRight: index % 2 == 0 ? const Radius.circular(20) : Radius.zero,
+                  ),
+                  // gradient: const LinearGradient(
+                  //   colors: [Color.fromARGB(255, 5, 23, 39), Color.fromARGB(200, 228, 211, 182),Color.fromARGB(255, 228, 211, 182),],
+                  //   begin: Alignment.centerLeft,
+                  //   end: Alignment.centerRight,
+                  // )
+                ),
+                imageBorderRadius: BorderRadius.only(
+                  topLeft: index % 2 == 0 ? const Radius.circular(19) : Radius.zero,
+                  topRight: index % 2 == 0 ? Radius.zero : const Radius.circular(19),
+                  bottomLeft: index % 2 == 0 ? Radius.zero : const Radius.circular(19),
+                  bottomRight: index % 2 == 0 ? const Radius.circular(19) : Radius.zero,
+                ),
               );
-            }
+              
+            },
           );
         }
       ) 
--- lib/presentation/screen/library/library.dart
+++ lib/presentation/screen/library/library.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:muix_player/helper/icons.dart';
-import 'package:muix_player/presentation/providers/tab_state_provider.dart';
 import 'package:muix_player/presentation/screen/albums/albums.dart';
 import 'package:muix_player/presentation/screen/all_songs/songs.dart';
 import 'package:muix_player/presentation/screen/artists/artist.dart';
@@ -13,6 +12,24 @@ import 'package:muix_player/theme/app_muix_theme.dart';
 import 'package:buttons_tabbar/buttons_tabbar.dart';
 import 'package:super_cupertino_navigation_bar/super_cupertino_navigation_bar.dart';
 
+const List<Tab> tabs = [
+  Tab(text: "Home",),
+  Tab(text: "Songs",),
+  Tab(text: "Albums",),
+  Tab(text: "Artist",),
+  Tab(text: "Genres",),
+  Tab(text: "Playlist"),
+];
+
+const List<Widget> tabsView = [
+  Dashboard(),
+  Songs(),
+  Albums(),
+  Artist(),
+  GenresScreen(),
+  PlaylistScreen()
+];
+
 class Library extends ConsumerStatefulWidget {
 const Library({ Key? key }) : super(key: key);
 
@@ -20,101 +37,68 @@ const Library({ Key? key }) : super(key: key);
   ConsumerState<Library> createState() => _LibraryState();
 }
 
-class _LibraryState extends ConsumerState<Library> with AutomaticKeepAliveClientMixin{
+class _LibraryState extends ConsumerState<Library> with AutomaticKeepAliveClientMixin, TickerProviderStateMixin{
 
-  late ScrollController scrollController;
+  late TabController controller;
+  final scrollController = ScrollController();
 
   @override
   void initState() {
-    scrollController = ScrollController();
+    controller = TabController(initialIndex: 0 ,length: tabs.length, vsync: this);
     super.initState();
   }
 
   @override
   Widget build(BuildContext context){
     super.build(context);
-    return SuperScaffold(
-      appBar: SuperAppBar(
-        title: Text('Library', style: AppMuixTheme.textTitleUrbanistRegular36,),
-        largeTitle: SuperLargeTitle(
-          largeTitle: 'Library',
-          textStyle: AppMuixTheme.textTitleUrbanistRegular36,
-          actions: [
-            ref.watch(tabStateProviderProvider) ? Container(
-              height: 35.w,
-              width: 35.w,
-              decoration: BoxDecoration(
-                color: const Color.fromARGB(255, 228, 211, 182),
-                border: Border.all(color: Colors.white),
-                borderRadius: BorderRadius.circular(10)
-              ),
-              child: IconButton(onPressed: () => ref.watch(tabStateProviderProvider.notifier).isSelected(false), icon: const Iconify(Jam.chevron_left))
-            ) : Container(),
-            IconButton(onPressed: (){}, icon: const Iconify(MaterialSymbols.folder_outline)),
-            IconButton(onPressed: (){}, icon: const Iconify(Carbon.equalizer)),
-            Container(
-              height: 35.w,
-              width: 35.w,
-              decoration: BoxDecoration(
-                color: const Color.fromARGB(255, 228, 211, 182),
-                border: Border.all(color: Colors.white),
-                borderRadius: BorderRadius.circular(10)
-              ),
-              child: IconButton(onPressed: (){}, icon: const Iconify(Jam.menu))
-            ),
-            
-          ]
-        ),
-        searchBar: SuperSearchBar(enabled: false),
-        backgroundColor: Colors.transparent,
-      ),
-      scrollController: scrollController,
-      body: Scaffold(
-        backgroundColor: Colors.transparent,
-        extendBody: true,
-        body: Padding(
-          padding: const EdgeInsets.only(top: 20),
-          child: DefaultTabController(
-            
-            length: 6,
-            
-            child: Column(
-              children: <Widget>[
-                ButtonsTabBar(
-                  height: 35,
-                  borderWidth: 1,
-                  unselectedBorderColor: Colors.white,
-                  borderColor: Colors.white,
-                  backgroundColor: AppMuixTheme.primary,
-                  unselectedBackgroundColor: AppMuixTheme.background,
-                  contentPadding: EdgeInsets.symmetric(horizontal: 25.w,),
-                  buttonMargin: EdgeInsets.symmetric(horizontal: 20.w,),
-                  unselectedLabelStyle: const TextStyle(color: Colors.black),
-                  labelStyle: const TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
-                  tabs: const [
-                    Tab(text: "Home",),
-                    Tab(text: "Songs",),
-                    Tab(text: "Albums",),
-                    Tab(text: "Artist",),
-                    Tab(text: "Genres",),
-                    Tab(text: "Playlist"),
-                  ],
+    return DefaultTabController(
+      length: tabs.length,
+      child: SuperScaffold(
+        stretch: false,
+        appBar: SuperAppBar(
+          title: Text('Library', style: AppMuixTheme.textTitleUrbanistRegular36,),
+          largeTitle: SuperLargeTitle(
+            largeTitle: 'Library',
+            textStyle: AppMuixTheme.textTitleUrbanistRegular36,
+            actions: [
+              IconButton(onPressed: (){}, icon: const Iconify(MaterialSymbols.folder_outline)),
+              IconButton(onPressed: (){}, icon: const Iconify(Carbon.equalizer)),
+              Container(
+                height: 35.w,
+                width: 35.w,
+                decoration: BoxDecoration(
+                  color: const Color.fromARGB(255, 228, 211, 182),
+                  border: Border.all(color: Colors.white),
+                  borderRadius: BorderRadius.circular(10)
                 ),
-                const Expanded(
-                  child: TabBarView(
-                    children: <Widget>[
-                      Dashboard(),
-                      Songs(),
-                      Albums(),
-                      Artist(),
-                      GenresScreen(),
-                      PlaylistScreen()
-                    ],
-                  ),
-                ),
-              ],
+                child: IconButton(onPressed: (){}, icon: const Iconify(Jam.menu))
+              ),
+            ]
+          ),
+          bottom: SuperAppBarBottom(
+            enabled: true,
+            child: ButtonsTabBar(
+              height: 35,
+              borderWidth: 1,
+              unselectedBorderColor: Colors.white,
+              borderColor: Colors.white,
+              backgroundColor: AppMuixTheme.primary,
+              unselectedBackgroundColor: AppMuixTheme.background,
+              contentPadding: EdgeInsets.symmetric(horizontal: 25.w,),
+              buttonMargin: EdgeInsets.symmetric(horizontal: 20.w,),
+              unselectedLabelStyle: const TextStyle(color: Colors.black),
+              labelStyle: const TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+              controller: controller,
+              tabs: tabs,
             ),
           ),
+          searchBar: SuperSearchBar(enabled: false),
+          backgroundColor: Colors.transparent,
+        ),
+        scrollController: scrollController,
+        body: TabBarView(
+          controller: controller,
+          children: tabsView,
         ),
       ),
     );
